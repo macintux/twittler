c0a28b738a0f92e02277007c2d3ccdd12b92f886
John Daily <git@epep.us>
2012-12-31 17:39:27 -0500

    Add search timeline-ish helper code. Need to refactor, too much copy/paste

--- src/twitter_helper.erl
+++ src/twitter_helper.erl
@@ -9,9 +9,49 @@
 %%% Created : 11 Dec 2012 by John Daily <jd@epep.us>
 
 -module(twitter_helper).
--export([timeline/3, extract_urls/1, extract_retweet/1]).
+-export([timeline/3, extract_urls/1, extract_retweet/1, search/3]).
 
 -define(MAX_TL_REQ, 200).
+-define(MAX_SEARCH_REQ, 100).
+
+%% A search is effectively same as a timeline, but the results aren't structured the same.
+%% Results: [{"statuses", [<list of tweets>]}, {search_metadata, [<list_of_metadata>]}]
+
+%% We could make the timeline() function more general by supplying
+%% parser functions and the API function to invoke for each iteration.
+
+
+
+%% @doc Take the specified search, return the specified number of
+%% messages as processed by Fun
+search(Query, {count, X}, Fun) when X > ?MAX_SEARCH_REQ ->
+    search(Query, X - ?MAX_SEARCH_REQ, ?MAX_SEARCH_REQ,
+           twitter_client:search(Query, [{count, ?MAX_SEARCH_REQ}, {result_type, "recent"}]), [], Fun);
+search(Query, {count, X}, Fun) ->
+    search(Query, 0, X,
+           twitter_client:search(Query, [{count, X}, {result_type, "recent"}]), [], Fun).
+
+%% Possible base cases:
+%%   1  We've run out of messages to retrieve
+%%        We'll know this happened when we receive fewer messages than we asked for on this run.  I think.
+%%   2  We've retrieved as many messages as originally asked for
+search(_Query, _Remaining, Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) when Requested > length(Latest) ->
+    lists:reverse(lists:append(lists:map(Fun, Latest), Accum));
+search(_Query, Remaining, _Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) when Remaining =< 0 ->
+    lists:reverse(lists:append(lists:map(Fun, Latest), Accum));
+search(Query, Remaining, Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) when Remaining < Requested ->
+    OldestId = find_id(lists:last(Latest)),
+    ToRequest = Remaining,
+    search(Query, 0, ToRequest,
+             twitter_client:search(Query, [{count, ToRequest}, {max_id, OldestId - 1}, {result_type, "recent"}]),
+             lists:append(lists:map(Fun, Latest), Accum), Fun);
+search(Query, Remaining, Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) ->
+    OldestId = find_id(lists:last(Latest)),
+    ToRequest = Requested,
+    search(Query, Remaining - ToRequest, ToRequest,
+             twitter_client:search(Query, [{count, ToRequest}, {max_id, OldestId - 1}, {result_type, "recent"}]),
+             lists:append(lists:map(Fun, Latest), Accum), Fun).
+
 
 %% @doc Take the specified timeline, return the specified number of
 %% messages as processed by Fun
