bf26f45d37579b2b6d8f10bd98f7fa2ba2cd37c2
John Daily <git@epep.us>
2013-01-01 14:06:34 -0500

    Significant cleanup on helper navigation of timeline, searches

--- src/twitter_helper.erl
+++ src/twitter_helper.erl
@@ -11,75 +11,63 @@
 -module(twitter_helper).
 -export([timeline/3, extract_urls/1, extract_retweet/1, search/3]).
 
+-export([test_timeline/0, test_search/0]).
+
 -define(MAX_TL_REQ, 200).
 -define(MAX_SEARCH_REQ, 100).
 
 %% A search is effectively same as a timeline, but the results aren't structured the same.
-%% Results: [{"statuses", [<list of tweets>]}, {search_metadata, [<list_of_metadata>]}]
+%% Search results: [{"statuses", [<list of tweets>]}, {search_metadata, [<list_of_metadata>]}]
+%% Timeline results: [<list of tweets>]
 
-%% We could make the timeline() function more general by supplying
-%% parser functions and the API function to invoke for each iteration.
-
-
+-record(state, {
+          desired, %% Which timeline we want, or search query. Need better word
+          api_call, %% twitter_client:timeline or twitter_client:search
+          results_parser, %% search and timeline return different types of lists
+          per_msg_fun, %% Client-provided function to run against each message
+          max_id=0, %% First message we encounter
+          min_id=0, %% Last message we encounter
+          args=[] %% Arguments to pass, not including count, max_id
+         }).
 
 %% @doc Take the specified search, return the specified number of
 %% messages as processed by Fun
 search(Query, {count, X}, Fun) when X > ?MAX_SEARCH_REQ ->
-    search(Query, X - ?MAX_SEARCH_REQ, ?MAX_SEARCH_REQ,
-           twitter_client:search(Query, [{count, ?MAX_SEARCH_REQ}, {result_type, "recent"}]), [], Fun);
+    {State, List} =
+        unified(X, ?MAX_SEARCH_REQ, [], [],
+            #state{desired = Query,
+                   api_call = fun twitter_client:search/2,
+                   results_parser = fun(Y) -> proplists:get_value(<<"statuses">>, Y) end,
+                   per_msg_fun = Fun}),
+    [ {max_id, State#state.max_id}, {min_id, State#state.min_id}, {tweets, List} ];
 search(Query, {count, X}, Fun) ->
-    search(Query, 0, X,
-           twitter_client:search(Query, [{count, X}, {result_type, "recent"}]), [], Fun).
-
-%% Possible base cases:
-%%   1  We've run out of messages to retrieve
-%%        We'll know this happened when we receive fewer messages than we asked for on this run.  I think.
-%%   2  We've retrieved as many messages as originally asked for
-search(_Query, _Remaining, Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) when Requested > length(Latest) ->
-    lists:reverse(lists:append(lists:map(Fun, Latest), Accum));
-search(_Query, Remaining, _Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) when Remaining =< 0 ->
-    lists:reverse(lists:append(lists:map(Fun, Latest), Accum));
-search(Query, Remaining, Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) when Remaining < Requested ->
-    OldestId = find_id(lists:last(Latest)),
-    ToRequest = Remaining,
-    search(Query, 0, ToRequest,
-             twitter_client:search(Query, [{count, ToRequest}, {max_id, OldestId - 1}, {result_type, "recent"}]),
-             lists:append(lists:map(Fun, Latest), Accum), Fun);
-search(Query, Remaining, Requested, [{<<"statuses">>, Latest}, {<<"search_metadata">>, _MD}], Accum, Fun) ->
-    OldestId = find_id(lists:last(Latest)),
-    ToRequest = Requested,
-    search(Query, Remaining - ToRequest, ToRequest,
-             twitter_client:search(Query, [{count, ToRequest}, {max_id, OldestId - 1}, {result_type, "recent"}]),
-             lists:append(lists:map(Fun, Latest), Accum), Fun).
+    {State, List} =
+        unified(X, X, [], [],
+            #state{desired = Query,
+                   api_call = fun twitter_client:search/2,
+                   results_parser = fun(Y) -> proplists:get_value(<<"statuses">>, Y) end,
+                   per_msg_fun = Fun}),
+    [ {max_id, State#state.max_id}, {min_id, State#state.min_id}, {tweets, List} ].
 
 
-%% @doc Take the specified timeline, return the specified number of
-%% messages as processed by Fun
 timeline(Which, {count, X}, Fun) when X > ?MAX_TL_REQ ->
-    timeline(Which, X - ?MAX_TL_REQ, ?MAX_TL_REQ, twitter_client:timeline(Which, [{count, ?MAX_TL_REQ}]), [], Fun);
+    {State, List} =
+        unified(X, ?MAX_TL_REQ, [], [],
+            #state{desired = Which,
+                   api_call = fun twitter_client:timeline/2,
+                   results_parser = fun(Y) -> Y end,
+                   per_msg_fun = Fun}),
+    [ {max_id, State#state.max_id}, {min_id, State#state.min_id}, {tweets, List} ];
 timeline(Which, {count, X}, Fun) ->
-    timeline(Which, 0, X, twitter_client:timeline(Which, [{count, X}]), [], Fun).
+    {State, List} =
+        unified(X, X, [], [],
+            #state{desired = Which,
+                   api_call = fun twitter_client:timeline/2,
+                   results_parser = fun(Y) -> Y end,
+                   per_msg_fun = Fun}),
+    [ {max_id, State#state.max_id}, {min_id, State#state.min_id}, {tweets, List} ].
 
-%% Possible base cases:
-%%   1  We've run out of messages to retrieve
-%%        We'll know this happened when we receive fewer messages than we asked for on this run.  I think.
-%%   2  We've retrieved as many messages as originally asked for
-timeline(_Timeline, _Remaining, Requested, Latest, Accum, Fun) when Requested > length(Latest) ->
-    lists:reverse(lists:append(lists:map(Fun, Latest), Accum));
-timeline(_Timeline, Remaining, _Requested, Latest, Accum, Fun) when Remaining =< 0 ->
-    lists:reverse(lists:append(lists:map(Fun, Latest), Accum));
-timeline(Timeline, Remaining, Requested, Latest, Accum, Fun) when Remaining < Requested ->
-    OldestId = find_id(lists:last(Latest)),
-    ToRequest = Remaining,
-    timeline(Timeline, 0, ToRequest,
-             twitter_client:timeline(Timeline, [{count, ToRequest}, {max_id, OldestId - 1}]),
-             lists:append(lists:map(Fun, Latest), Accum), Fun);
-timeline(Timeline, Remaining, Requested, Latest, Accum, Fun) ->
-    OldestId = find_id(lists:last(Latest)),
-    ToRequest = Requested,
-    timeline(Timeline, Remaining - ToRequest, ToRequest,
-             twitter_client:timeline(Timeline, [{count, ToRequest}, {max_id, OldestId - 1}]),
-             lists:append(lists:map(Fun, Latest), Accum), Fun).
+
 
 find_id(Tweet) ->
     proplists:get_value(<<"id">>, Tweet).
@@ -98,3 +86,100 @@ extract_retweet(Tweet, undefined) ->
     Tweet;
 extract_retweet(_Tweet, Retweet) ->
     Retweet.
+
+
+%% Consolidate timeline, search
+%% unified() returns { State, List }
+
+%% Entry point
+unified(Remaining, Requested, [], [], State) ->
+    Tweets = do_call(Requested, State),
+    FirstId = find_id(lists:nth(1, Tweets)),
+    unified(Remaining - Requested, Requested, Tweets, [],
+            State#state{max_id=FirstId});
+
+%% Asked for more than we received, stop.
+unified(_Remaining, Requested, Latest, Accum, State) when Requested > length(Latest) ->
+    process_latest(Latest, Accum, State);
+
+%% Typical ending point, no more to ask for.
+unified(Remaining, _Requested, Latest, Accum, State) when Remaining =< 0 ->
+    process_latest(Latest, Accum, State);
+
+%% Last batch. Ask only for the remainder, not the max count
+unified(Remaining, Requested, Latest, Accum, State) when Remaining =< Requested ->
+    {NewState, NewAccum} = process_latest(Latest, Accum, State),
+    unified(0, Remaining,
+            do_call(Remaining, NewState), NewAccum, NewState);
+
+%% Intermediate invocation when count > Twitter max allowed
+unified(Remaining, Requested, Latest, Accum, State) ->
+    {NewState, NewAccum} = process_latest(Latest, Accum, State),
+    unified(Remaining - Requested, Requested,
+            do_call(Requested, NewState),
+            NewAccum, NewState).
+
+
+do_call(NextQty, State) ->
+    do_call(NextQty, State, State#state.min_id).
+
+%% If we don't have a minimum ID yet, that means we start at the first
+%% tweet Twitter will give us.
+do_call(Qty, State, 0) ->
+    apply(State#state.results_parser,
+          [ apply(State#state.api_call,
+                [State#state.desired,
+                 State#state.args ++
+                     [{count, Qty}]])
+          ]
+         );
+%% If we do have a minimum ID from previous timeline/search
+%% navigation, use that (minus 1) as the maximum ID we want Twitter to
+%% give us the second time around
+do_call(Qty, State, MinId) ->
+    apply(State#state.results_parser,
+          [ apply(State#state.api_call,
+                [State#state.desired,
+                 State#state.args ++
+                     [{count, Qty},
+                      {max_id, MinId - 1}]])
+          ]
+          ).
+
+%% Utility function to take the latest results, merge them with the
+%% existing results, and update state appropriately
+process_latest(Latest, Accum, State) ->
+    OldestId = find_id(lists:last(Latest)),
+    { State#state{min_id=OldestId},
+      lists:append(Accum,
+                   lists:map(State#state.per_msg_fun, Latest)) }.
+
+%%%%%% Test suite, should make eunit-compatible
+test_timeline() ->
+    ResultSet = timeline(user, {count, ?MAX_TL_REQ * 2 + 3}, fun(X) -> proplists:get_value(<<"id">>, X) end),
+    MaxId = proplists:get_value(max_id, ResultSet),
+    MinId = proplists:get_value(min_id, ResultSet),
+    {MaxId, MinId, ?MAX_TL_REQ * 2 + 3} = process_test_list(proplists:get_value(tweets, ResultSet)).
+
+test_search() ->
+    ResultSet = search("java", {count, ?MAX_SEARCH_REQ * 2 + 3}, fun(X) -> proplists:get_value(<<"id">>, X) end),
+    MaxId = proplists:get_value(max_id, ResultSet),
+    MinId = proplists:get_value(min_id, ResultSet),
+    {MaxId, MinId, ?MAX_SEARCH_REQ * 2 + 3} = process_test_list(proplists:get_value(tweets, ResultSet)).
+
+process_test_list(List) ->
+    ok = check_decrement(List),
+    {lists:nth(1, List),
+     lists:last(List),
+     length(List)}.
+
+check_decrement([H|T]) ->
+    check_decrement(T, H).
+
+%% Make sure the elements in a numerical list are ordered from highest to lowest
+check_decrement([], _Prev) ->
+    ok;
+check_decrement([H|_T], Prev) when H > Prev ->
+    fail;
+check_decrement([_H|T], Prev) ->
+    check_decrement(T, Prev).
